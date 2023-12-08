2b0386d733ac03226465ed209277557dae64d833
Ile83 <ilkka@heusala.fi>
2023-12-08 14:44:44 +0200

    Wordplay exercise, not graded

--- src/main/java/wordplay/NamesInDictionary.java
+++ src/main/java/wordplay/NamesInDictionary.java
@@ -1,6 +1,9 @@
 package wordplay;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import wordplay.io.DictionaryReader;
 import wordplay.io.NamesReader;
@@ -25,5 +28,24 @@ public class NamesInDictionary {
          *
          * Good luck!
          */
+           // Convert all names to lower case
+    List<String> lowerCaseNames = finnishNames.stream()
+        .map(String::toLowerCase)
+        .collect(Collectors.toList());
+
+    // Remove duplicates
+    List<String> uniqueNames = lowerCaseNames.stream()
+        .distinct()
+        .collect(Collectors.toList());
+
+    // Use a HashSet for the dictionary words for efficient lookup
+    Set<String> wordsSet = new HashSet<>(finnishWords);
+
+    // Iterate over the names and check if they are in the dictionary
+    for (String name : uniqueNames) {
+        if (wordsSet.contains(name)) {
+            System.out.println(name);
+        }
     }
+}
 }
